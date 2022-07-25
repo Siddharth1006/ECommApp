9526e559fd56e2c4c7de9800e8a01946b4e30337
Siddharth S <siddharthsoma01@gmail.com>
2022-07-26 04:43:36 +0530

    Components Lining Up issue Added 3 more components. But they line up one after the other. We want them to line up side by side.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,12 +9,19 @@ const Container = styled.div`
 const Wrapper = styled.div`
   padding: 10px 20px
 `
+
+const Left = styled.div``
+const Center = styled.div``
+const Right = styled.div``
+
 const Navbar = () => {
   return (
     <Container>
         <Wrapper>
           {/* Adds space from top and bottom */}
-            navbar
+            <Left> Left </Left>
+            <Right> Right </Right>
+            <Center> Center </Center>
         </Wrapper>
     </Container>
   )
