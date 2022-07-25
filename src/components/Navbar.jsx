b12b771a56131ff66b91c3caf2152821e43cfee9
Siddharth S <siddharthsoma01@gmail.com>
2022-07-26 04:53:35 +0530

    Used Flexbox to align the components in Navbar side by side(horizontally)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,24 +4,28 @@ import styled from 'styled-components';
 //We are creating our own style components and WILL NOT BE USING CSS
 const Container = styled.div`
   height: 65px;
-`
+`;
 
 const Wrapper = styled.div`
-  padding: 10px 20px
-`
+  padding: 10px 20px;
 
-const Left = styled.div``
-const Center = styled.div``
-const Right = styled.div``
+  /*Using Flex box here to make the Wrapper contents horizontal*/
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Left = styled.div``;
+const Center = styled.div``;
+const Right = styled.div``;
 
 const Navbar = () => {
   return (
     <Container>
+            {/* Wrapper component is used to Wrap the navbar components like login , register etc. */}
         <Wrapper>
-          {/* Adds space from top and bottom */}
             <Left> Left </Left>
-            <Right> Right </Right>
             <Center> Center </Center>
+            <Right> Right </Right>
         </Wrapper>
     </Container>
   )
