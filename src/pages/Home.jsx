9ec2f217d6966e8a8837c7ee074575ae86d9e77f
Siddharth S <siddharthsoma01@gmail.com>
2022-07-26 04:03:09 +0530

    Added some more files and cleaned up the directory. Make sure <div id=root></div> is present in index.html to render the changes made to be displayed in React App.

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+//We are creating our own style components
+const Container = 
+
+console.log("Hello World");
+const Home = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Home;
