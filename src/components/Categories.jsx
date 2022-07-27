25e158c6b4dd32b666af7205f46de16c0edf91ea
Siddharth S <siddharthsoma01@gmail.com>
2022-07-27 18:28:38 +0530

    Added Categories and we can see the test run. Since we have 3 objects in categories list hello pops up thrice at the bottom as the categoryItem is fetched thrice. Use Reference7 image to have look.

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
-import styled from "@mui/material"
+import styled from "styled-components"
 import { categories } from  "../data"
 import { CategoryItem } from './CategoryItem';
 
 const Container = styled.div`
+     display: flex;
+     padding: 30px;
+     justify-content: space-between;
 `;
 
 const Categories = () => {
--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
-import { styled } from "@mui/material"
+import styled  from "styled-components"
 
-const Container = styled.div``;
+const Container = styled.div`
+
+`;
 
 //Each category item is being fetched from here to the Categories
 export const CategoryItem = ({item}) => {
   return (
     <Container>
-
+        HEHLLLO!!
     </Container>
   )
 }
