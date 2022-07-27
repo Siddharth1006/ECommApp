40641fa36f64c82c22838f20bf83c4641ffde979
Siddharth S <siddharthsoma01@gmail.com>
2022-07-28 04:25:04 +0530

    flex-direction: column
    1. Adding the above command made the horizontal styled components -
    Title, Description, InputContainer etc. in vertical order.
    2. Rest is simple to follow.

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -5,16 +5,27 @@ import { Send } from '@mui/icons-material'
 import styled from 'styled-components'
 
 const Container = styled.div`
-    height: 60vh;
+    height: 58vh;
     background-color: #fcf5f5;
     
     display: flex;
     align-items: center;
     justify-content: center;
+
+    flex-direction: column;
 `;
 
-const Title = styled.h1``;
-const Description = styled.div``;
+const Title = styled.h1`
+    font-size: 72px;
+    margin-bottom: 20px;
+`;
+
+const Description = styled.div`
+    font-size: 30px;
+    font-weight: 300;
+    margin-bottom: 24px;
+`;
+
 const InputContainer = styled.div``;
 const Input = styled.input``;
 const Button = styled.button``;
