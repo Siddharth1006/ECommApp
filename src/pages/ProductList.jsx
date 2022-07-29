743df82622f53fe835fc039f9fdbeed9f13730da
Siddharth S <siddharthsoma01@gmail.com>
2022-07-29 06:10:01 +0530

    Added Filter Products and Sort Products DropDown

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -25,6 +25,9 @@ const FilterText = styled.span`
     font-weight: 600;
 `;
 
+const Select = styled.select``;
+const Option = styled.option``;
+
 function ProductList() {
   return (
         <Container>
@@ -36,10 +39,33 @@ function ProductList() {
             <FilterContainer>
                 <Filter> 
                     <FilterText> Filter Products: </FilterText>
+                    <Select>
+                        <Option disabled selected> Color </Option>
+                        <Option>White</Option>
+                        <Option>Black</Option>
+                        <Option>Red</Option>
+                        <Option>Blue</Option>
+                        <Option>Yellow</Option>
+                        <Option>Green</Option>
+                    </Select>
+
+                    <Select>
+                        <Option disabled selected> Size </Option>
+                        <Option>XS</Option>
+                        <Option>S</Option>
+                        <Option>M</Option>
+                        <Option>L</Option>
+                        <Option>XL</Option>
+                    </Select>
                 </Filter>
 
                 <Filter>
                     <FilterText> Sort Products: </FilterText>
+                    <Select>
+                        <Option selected>Newest</Option>
+                        <Option>Price (asc)</Option>
+                        <Option>Price (desc)</Option>
+                    </Select>
                 </Filter>
             </FilterContainer> 
 
